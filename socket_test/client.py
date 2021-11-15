033444330547b56e63de1f42c2eb8e54b583a308
vahidmohsseni <vahidmohsseni@yahoo.com>
2021-11-15 18:13:49 +0200

    Add test files for sockets

---
+++ socket_test/client.py
@@ -0,0 +1,38 @@
+import socket
+import time
+import random
+
+SERVERADDRESS = ("192.168.10.2", 9000)
+
+
+
+def main():
+    s = socket.socket()
+    print("connect to server: ", SERVERADDRESS)
+    s.connect(SERVERADDRESS)
+
+    rand = random.randrange(5, 15)
+
+    message = b"Hello from the other side!"
+
+    while True:
+        time.sleep(1)
+        s.sendall(message)
+        print("message sent!")
+
+        echo = s.recv(1024)
+        print(echo)
+
+        rand -= 1
+        if rand == 0:
+            message = b'terminate'
+
+        if echo == b'terminate':
+            s.close()
+            break
+
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ socket_test/server.py
@@ -0,0 +1,47 @@
+from os import close
+import socket
+import threading
+
+
+SERVERADDRESS = ("0.0.0.0", 9000)
+
+def echo(connection, message):
+
+    return 1
+
+
+def receive_message(connection, client_address):
+    while True:
+        message = connection.recv(1024)
+        print("from: ", client_address, "message: ", message)
+        connection.send(message)
+        
+        if message == b'terminate':
+            connection.close()
+            break
+            
+
+
+
+def main():
+
+    s = socket.socket()
+
+    s.bind(SERVERADDRESS)
+
+    s.listen(5)
+
+    while True:
+        print("Waiting for a connection ...")
+
+        connection, client_addr = s.accept()
+
+        print("connection received from: ", client_addr)
+
+        th = thread = threading.Thread(target=receive_message, args=[connection, client_addr])
+        th.start()
+
+
+if __name__ == "__main__":
+    main()
+
